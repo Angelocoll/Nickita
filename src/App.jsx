a187bb2804d76c85e3d47e51817a3ec721f5bf2c
Angelocoll <angelo.collocolo@yh.nackademin.se>
2025-01-28 15:00:21 +0100

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import Startsidan from "./Component/Startsidan";
 import img from "./img/IMG.jpg";
 
-
 function App() {
+  // Dynamiskt sätt var(--vh) för att fixa iOS 100vh-problemet
+  useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    // Kör funktionen direkt och när fönstret ändras
+    setVh();
+    window.addEventListener("resize", setVh);
+
+    // Rensa event listener vid unmount
+    return () => window.removeEventListener("resize", setVh);
+  }, []);
+
   return (
     <>
-    <div className="shadow">
-    <div className="img"><img src={img} alt="" /></div>
-    </div>
-    
-    <Startsidan/>
+      <div className="shadow">
+        <img className="img" src={img} alt="Background" />
+      </div>
+
+      {/* Lägg Startsidan-komponenten */}
+      <Startsidan />
     </>
   );
 }
