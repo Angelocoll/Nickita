74a3a46ae33ba62ef1eb9405eea2dea53a59d66b
Angelocoll <angelo.collocolo@yh.nackademin.se>
2025-01-28 14:44:29 +0100

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import img from "./img/IMG.jpg";
 
 function App() {
   return (
-    <div className="root">
+    <>
     <div className="shadow">
     <div className="img"><img src={img} alt="" /></div>
     </div>
     
     <Startsidan/>
-    </div>
+    </>
   );
 }
 
